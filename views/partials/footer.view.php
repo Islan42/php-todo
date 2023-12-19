0bd900a1f07923667388b942ff42648df4f9a378
Islan <islan12victhor@gmail.com>
2023-12-19 07:51:55 -0300

    Footer

--- views/partials/footer.view.php
+++ views/partials/footer.view.php
@@ -1,7 +1,7 @@
 
 		</main>
 
-		<footer class="bg-gray-500 h-16 p-2 text-gray-200">
+		<footer class="bg-gray-600 h-16 p-2 text-gray-200">
 			<p>Desenvolvido por Islan Victhor</p>
 		</footer>
 	</body>
