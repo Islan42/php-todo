cbf40d3bf5c4a5d117a6b10176bc6df684520720
Islan <islan12victhor@gmail.com>
2023-12-25 11:28:24 -0300

    HTMLSpecialChars()

--- views/index.view.php
+++ views/index.view.php
@@ -4,6 +4,6 @@
 	<?php if (! Core\Session::has('user')): ?>
 		<p>Seja bem vindo à Homepage.</p>
 	<?php else: ?>
-		<p>Ben vindo, <?= Core\Session::get('user')['name'] ?>.</p>
+		<p>Ben vindo, <?= htmlspecialchars(Core\Session::get('user')['name']) ?>.</p>
 		<p>Você pode acessar sua <a href="/todos" class="text-blue-500 hover:underline">lista de A Fazeres</a>.</p>
 	<?php endif; ?>
--- views/partials/head.view.php
+++ views/partials/head.view.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width,initial-scale=1" />
-	<title>ToDo App</title>
+	<title>A Fazeres</title>
 	<script src="https://cdn.tailwindcss.com"></script>
 	<style type="text/tailwindcss">
 		@layer components {
