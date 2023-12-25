1a5e4e0220935f0b70e551e5ef2c700b9c8255ff
Islan <islan12victhor@gmail.com>
2023-12-25 08:18:43 -0300

    Finalizei o Footer

--- views/partials/footer.view.php
+++ views/partials/footer.view.php
@@ -1,8 +1,22 @@
 
 		</main>
 
-		<footer class="bg-slate-600 min-h-16 p-2 text-gray-200">
-			<p>Desenvolvido por Islan Victhor</p>
+		<footer class="bg-slate-600 min-h-16 p-2 text-gray-200  flex divide-x">
+			<div class="flex-1  text-center">
+				<p class="text-sm">Desenvolvido por Islan Victhor</p>
+				<p class="text-sm text-blue-400"><a href="https://github.com/Islan42">Github</a></p>
+				<p class="text-sm text-blue-400"><a href="https://www.linkedin.com/in/islan-v-426b17133/">LinkedIn</a></p>
+			</div>
+			<div class="p-2  flex-1  flex justify-center items-center">
+				<ul class="flex items-center  text-red-500 space-x-4">
+					<li><a href="https://www.php.net/"><img src="/static/img/php-power-white.png" class="max-h-6" /></a></li>
+					<li><a href="https://httpd.apache.org/"><img src="/static/img/httpd-pb.png" class="max-h-10"/></a></li>
+					<li><a href="https://www.mysql.com/"><img src="/static/img/powered-by-mysql-88x31.png" class="max-h-6" /></a></li>
+					<li><a href="https://www.docker.com/"><img src="/static/img/01-symbol_blue-docker-logo.png" class="max-h-6" /></a></li>
+					<li><a href="https://react.dev/"><img src="/static/img/react-logo.svg" class="h-6" /></a></li>				
+					<li class="flex flex-col justify-center"><a href="https://tailwindcss.com/" class="text-xs text-sky-400 font-bold italic">tailwindcss</a></li>
+				</ul>
+			</div>
 		</footer>
 	</body>
 </html>
